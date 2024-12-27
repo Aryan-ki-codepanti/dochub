b716c8a71d98e61adfe3798ad25aacf1605f5986
Aryan Sethi <aryan.rdps@gmail.com>
2024-12-27 23:00:37 +0530

    added user's profile getting endpoint

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -2,6 +2,16 @@ import asyncHandler from "express-async-handler";
 import { filesize } from "filesize";
 import FileModel from "../models/fileModel.js";
 
+//@description     GET files from /uploads/userId  directory
+//@route           GET /api/files/
+//@access          Protected
+const getFilesInfo = asyncHandler(async (req, res) => {
+    const data = await FileModel.find({ owner: req.user._id }).sort({
+        updatedAt: -1
+    });
+    res.status(200).json(data);
+});
+
 //@description     Upload 1 to 5 files to /uploads/userId  directory
 //@route           POST /api/files/upload
 //@access          Protected
@@ -27,4 +37,4 @@ const uploadFiles = asyncHandler(async (req, res) => {
     res.json(fileData);
 });
 
-export { uploadFiles };
+export { getFilesInfo, uploadFiles };
--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -2,10 +2,11 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 
 import upload from "../config/multerConfigure.js";
-import { uploadFiles } from "../controllers/fileController.js";
+import { getFilesInfo, uploadFiles } from "../controllers/fileController.js";
 
 const router = express.Router();
 
+router.get("/", protect, getFilesInfo);
 router.post("/upload", protect, upload.array("files", 5), uploadFiles);
 
 export default router;
